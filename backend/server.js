6553493ed79b191489f734a7fdae3684c43c6bd8
rowgregory <rowgregory@gmail.com>
2022-03-17 14:41:09 -0400

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome to Little Paws API');
-});
+const __dirname = path.resolve();
+
+app.get('/api', (req, res) =>
+  res.status(200).send('WELCOME TO LITLLE PAWS API')
+);
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('ping');
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).send('Welcome to Little Paws API');
+  });
+}
 
 app.listen(5000, console.log(`Server running on port 5000`));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 
 const App = () => {
+  const [greeting, setGreeting] = useState('');
+  useEffect(() => {
+    const getGreeting = async () => {
+      const { data } = await axios.get('/api');
+      setGreeting(data);
+    };
+
+    getGreeting();
+  }, []);
+  console.log(greeting);
   return (
     <div className='App'>
       <Header />
-      LPDR
+      {greeting}
     </div>
   );
 };
